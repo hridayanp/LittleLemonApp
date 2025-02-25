2f9b0b844c9f8a18f3d17abf3a91df281b768176
hridayanp <hridayanphukan123@gmail.com>
2025-02-25 11:06:18 +0530

    Initial Commit

---
+++ LittleLemonApp/LittleLemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonAppApp.swift
+//  LittleLemonApp
+//
+//  Created by Hridayan Phukan on 25/02/25.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LittleLemonAppTests/LittleLemonAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonAppTests.swift
+//  LittleLemonAppTests
+//
+//  Created by Hridayan Phukan on 25/02/25.
+//
+
+import Testing
+@testable import LittleLemonApp
+
+struct LittleLemonAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
